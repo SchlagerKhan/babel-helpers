280d2f487557af925738a2aa58886c9f3527bed9
Kalle <kh.hansson@gmail.com>
2019-11-30 10:57:57 +0100

    Updated styled components plugin

--- packages/babel-preset-js/index.js
+++ packages/babel-preset-js/index.js
@@ -1,53 +1,61 @@
-const defaultTargets = '> 0.25%, not dead';
-const defaultExtensions = ['.js'];
+const defaultTargets = "> 0.25%, not dead";
+const defaultExtensions = [".js"];
+
+const IS_DEV = process.env.NODE_ENV === "development";
 
 module.exports = function(api, opts) {
-	const {
-		// prettier-ignore
-		targets = defaultTargets,
-		extensions = defaultExtensions,
-		alias = {},
-		includeMetadata,
-		includeStyledComponents,
-	} = opts;
+  const {
+    // prettier-ignore
+    targets = defaultTargets,
+    extensions = defaultExtensions,
+    alias = {},
+    includeMetadata,
+    includeStyledComponents
+  } = opts;
 
-	const plugins = [
-		'@babel/plugin-transform-runtime',
-		'@babel/plugin-proposal-optional-chaining',
-		'@babel/plugin-proposal-export-default-from',
-		['@babel/plugin-proposal-decorators', { legacy: true }],
-		['@babel/plugin-proposal-class-properties', { loose: true }],
-		[
-			'babel-plugin-module-resolver',
-			{
-				root: ['.'],
-				extensions,
-				alias,
-			},
-		],
-		[
-			'babel-plugin-import',
-			{
-				libraryName: 'antd',
-				style: 'css',
-			},
-		],
-	];
+  const plugins = [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-export-default-from",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    [
+      "babel-plugin-module-resolver",
+      {
+        root: ["."],
+        extensions,
+        alias
+      }
+    ],
+    [
+      "babel-plugin-import",
+      {
+        libraryName: "antd",
+        style: "css"
+      }
+    ]
+  ];
 
-	if (includeMetadata) {
-		plugins.push('babel-plugin-transform-typescript-metadata');
-	}
+  if (includeMetadata) {
+    plugins.push("babel-plugin-transform-typescript-metadata");
+  }
 
-	if (includeStyledComponents) {
-		plugins.push('babel-plugin-styled-components');
-	}
+  if (includeStyledComponents) {
+    plugins.push([
+      "babel-plugin-styled-components",
+      {
+        displayName: IS_DEV,
+        minify: !IS_DEV
+      }
+    ]);
+  }
 
-	return {
-		presets: [
-			// prettier-ignore
-			['@babel/env', { targets }],
-			'@babel/react',
-		],
-		plugins,
-	};
+  return {
+    presets: [
+      // prettier-ignore
+      ['@babel/env', { targets }],
+      "@babel/react"
+    ],
+    plugins
+  };
 };
