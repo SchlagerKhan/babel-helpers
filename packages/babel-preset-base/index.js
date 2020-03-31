bbc27d19c7c5dedcd290162d9393814e00bd5353
Kalle <kh.hansson@gmail.com>
2020-03-31 11:29:36 +0200

    Moved regenerator-runtime plugin to base

--- packages/babel-preset-base/index.js
+++ packages/babel-preset-base/index.js
@@ -16,6 +16,7 @@ module.exports = function (api, opts) {
 	const { root, alias, extensions } = resolver;
 
 	const plugins = [
+		'@babel/plugin-transform-runtime',
 		['@babel/plugin-proposal-decorators', { legacy: true }],
 		['@babel/plugin-proposal-class-properties', { loose: true }],
 	];
--- packages/babel-preset-js/index.js
+++ packages/babel-preset-js/index.js
@@ -1,7 +1,6 @@
 module.exports = function (api, opts) {
 	const plugins = [
 		// prettier-ignore
-		'@babel/plugin-transform-runtime',
 		'@babel/plugin-proposal-export-default-from',
 	];
 
